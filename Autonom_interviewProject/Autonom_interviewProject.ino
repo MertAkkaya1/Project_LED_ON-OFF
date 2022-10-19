be3cd755460ce3de902792df071fac047f5cec95
MertAkkaya1 <85848855+MertAkkaya1@users.noreply.github.com>
2022-10-19 12:19:31 +0300

    Update Autonom_interviewProject

--- Autonom_interviewProject/Autonom_interviewProject.ino
+++ Autonom_interviewProject/Autonom_interviewProject.ino
@@ -3,7 +3,13 @@
 String okunan; 
 String LedOn = "ledon=";
 String LedOff = "ledoff=";
+String Start = "start";
+String Stop = "stop";
 String Okunan_Substring;
+String serial_read_Substring;
+String serial_read_Substring_On;
+String serial_read_Substring_Off;
+String serial_read;
 
 int Int_On_delay = -1;
 int Int_Off_delay = -1;
@@ -11,33 +17,56 @@ int Int_Off_delay = -1;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("hello");
+  
   pinMode(Ledpin, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0) { 
-    okunan = Serial.readString(); 
-      
-    if(okunan.substring(0,6) == LedOn){ 
-      Okunan_Substring = okunan.substring(6,okunan.length());
-      Int_On_delay = Okunan_Substring.toInt();
-      Serial.print("Led On ms: "); 
-      delay(Int_On_delay);  
+  if (Serial.available() > 0) {
+    okunan = Serial.readString();
+  }       
+  if (okunan.substring(0, 4) == Stop) {
+    //Serial.println(okunan);
+    Normal_Mode();
+  }
+  if (okunan.substring(0, 5) == Start) {
+    Echo_Mode();
+  }
+}
+
+void Normal_Mode(){
+  Serial.println("Normal Mode Active");
+  digitalWrite(Ledpin, HIGH);
+  delay(1000);
+  digitalWrite(Ledpin, LOW);
+  delay(1000);
+}
+
+void Echo_Mode(){
+  Serial.println("Echo Mode Active");
+
+  if (Serial.available() > 0) {
+    serial_read = Serial.readString();
+    if (serial_read.substring(0, 6) == LedOn) {
+      serial_read_Substring_On = serial_read.substring(6, serial_read.length());
+      Int_On_delay = serial_read_Substring_On.toInt();
+      Serial.print("Led On ms: ");
+      //delay(Int_On_delay);
       Serial.println(Int_On_delay);
     }
-    else if(okunan.substring(0,7) == LedOff){
-      Okunan_Substring = okunan.substring(7,okunan.length());
-      Int_Off_delay = Okunan_Substring.toInt();
-      Serial.print("Led Off ms: "); 
-      delay(Int_Off_delay);      
+    //serial_read = Serial.readString();
+    else if (serial_read.substring(0, 7) == LedOff) {
+      serial_read_Substring_Off = serial_read.substring(7, serial_read.length());
+      Int_Off_delay = serial_read_Substring_Off.toInt();
+      Serial.print("Led Off ms: ");
+      //delay(Int_Off_delay);
       Serial.println(Int_Off_delay);
-      }         
     }
-    if(Int_On_delay != -1 && Int_Off_delay != -1){
+    if (Int_On_delay != -1 && Int_Off_delay != -1) {
       digitalWrite(Ledpin, HIGH);
       delay(Int_On_delay);
       digitalWrite(Ledpin, LOW);
       delay(Int_Off_delay);
-    } 
+    }
+  }
 }
