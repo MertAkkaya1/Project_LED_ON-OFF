07dc91e8117e089215449e228e90d840a7ad74ab
MertAkkaya1 <85848855+MertAkkaya1@users.noreply.github.com>
2022-10-19 21:50:08 +0300

    Update Autonom_interviewProject

--- Autonom_interviewProject/Autonom_interviewProject.ino
+++ Autonom_interviewProject/Autonom_interviewProject.ino
@@ -1,3 +1,15 @@
+/*
+   Hazırlayan: Mert Akkaya
+   m.mertakkaya.a@gmail.com
+
+   Asagidaki kod blogu seri port ekranindan alinan verilere gore Led ON/OFF sürelerini ayarlamak icin yazilmistir.
+
+   Seri porttan girilen 'stop' komutu Led'in 1sn araliklarla yanip sonmesini saglar
+   Seri porttan girilen 'start' komutu Led'in bir onceki on/off surelerine gore yanip sonmesini saglar. Sonrasında bu on/off sureleri 'ledon=ON time' ve 'ledoff=OFF time' degerleri ile seri port uzerinden degistirilebilmektedir.
+
+   Seri haberlesme Serial.begin(115200, SERIAL_8N1); olarak default baslayip sonrasinda yine seri port uzerinden ayarlanabilmektedir.
+*/
+
 /* Macro defines */
 #define LEDPIN 2
 #define LED_ON_TIME 1000
@@ -13,6 +25,31 @@ enum Mode{
 /* Global variable definitions */
 enum Mode Mode_Select = NONE;
 
+String SeRIAL_5N1 = "SERIAL_5N1\n";
+String SeRIAL_6N1 = "SERIAL_6N1\n";
+String SeRIAL_7N1 = "SERIAL_7N1\n";
+String SeRIAL_8N1 = "SERIAL_8N1\n";
+String SeRIAL_5N2 = "SERIAL_5N2\n";
+String SeRIAL_6N2 = "SERIAL_6N2\n";
+String SeRIAL_7N2 = "SERIAL_7N2\n";
+String SeRIAL_8N2 = "SERIAL_8N2\n";
+String SeRIAL_5E1 = "SERIAL_5E1\n";
+String SeRIAL_6E1 = "SERIAL_6E1\n";
+String SeRIAL_7E1 = "SERIAL_7E1\n";
+String SeRIAL_8E1 = "SERIAL_8E1\n";
+String SeRIAL_5E2 = "SERIAL_5E2\n";
+String SeRIAL_6E2 = "SERIAL_6E2\n";
+String SeRIAL_7E2 = "SERIAL_7E2\n";
+String SeRIAL_8E2 = "SERIAL_8E2\n";
+String SeRIAL_5O1 = "SERIAL_5O1\n";
+String SeRIAL_6O1 = "SERIAL_6O1\n";
+String SeRIAL_7O1 = "SERIAL_7O1\n";
+String SeRIAL_8O1 = "SERIAL_8O1\n";
+String SeRIAL_5O2 = "SERIAL_5O2\n";
+String SeRIAL_6O2 = "SERIAL_6O2\n";
+String SeRIAL_7O2 = "SERIAL_7O2\n";
+String SeRIAL_8O2 = "SERIAL_8O2\n";
+
 String Data;
 String LedOn = "ledon=";
 String LedOff = "ledoff=";
@@ -22,10 +59,12 @@ String Data_On;
 String Data_Off;
 String Data_Substring_On;
 String Data_Substring_Off;
+String Data_Baud_Rate_Str;
 
 int Int_On_delay = -1;
 int Int_Off_delay = -1;
 int Data_Baud_Rate_Int = 0;
+int Date_Baud_Rate_StrToInt = -1;
 
 bool start_flag = false;
 
@@ -47,14 +86,24 @@ void loop() {
   if (Serial.available()) {
     Data = Serial.readString();
 
-    if (Data.substring(0, 3) == "300" || Data.substring(0, 3) == "600" || Data.substring(0, 4) == "1200" || Data.substring(0, 4) == "2400" || Data.substring(0, 4) == "4800" || Data.substring(0, 4) == "9600" || Data.substring(0, 5) == "19200" || Data.substring(0, 5) == "18400" || Data.substring(0, 6) == "115200") {
-      Data_Baud_Rate_Int = Data.toInt();
+    if (Data.substring(0, 3) == "300" || Data.substring(0, 3) == "600" || Data.substring(0, 4) == "1200" || Data.substring(0, 4) == "2400" || Data.substring(0, 15) == "4800" || Data.substring(0, 15) == "9600" || Data.substring(0, 5) == "19200" || Data.substring(0, 5) == "18400" || Data.substring(0, 6) == "115200") {
+      for (int i = 0; i < Data.length(); i++) {
+        if (Data.substring(i, i + 1) == " ") {
+          Data_Baud_Rate_Int = Data.substring(0, i).toInt(); // example: 9600
+          Data_Baud_Rate_Str = Data.substring(i + 1);       // example: SERIAL_8N1
+          //Serial.println(Data_Baud_Rate_Int);
+          //Serial.println(Data_Baud_Rate_Int);
+          //Serial.println(Data_Baud_Rate_Str);
+          Date_Baud_Rate_StrToInt = Baud_Rate_Str_Argument(Data_Baud_Rate_Str);
+          Echo_Mode(String(Data_Baud_Rate_Int), Data_Baud_Rate_Str);
+          //Serial.print("Baud Rate Function: ");
+          //Serial.println(Date_Baud_Rate_StrToInt);;
+        }        
+      }
       Serial.end();
-      Serial.begin(Data_Baud_Rate_Int);
-      //Serial.print("New Baud Rate:");
-      //Serial.println(Data_Baud_Rate_Int);
+      Serial.begin(Data_Baud_Rate_Int, Date_Baud_Rate_StrToInt);
     }
-
+    
     if (Data.substring(0, 4) == Stop) {
       Mode_Select = STOP;
       start_flag = false;
@@ -118,4 +167,80 @@ void Echo_Mode(String On_time_string, String Off_time_string)
   //Serial.println("Task2 Active");
   Serial.println(On_time_string);
   Serial.println(Off_time_string);
+}
+
+int Baud_Rate_Str_Argument(String argument) {
+  //Serial.println("Baud_Rate_Str_Argument");
+  if (argument == SeRIAL_5N1) {
+    return 0x00;
+  }
+  else if (argument == SeRIAL_6N1) {
+    return 0x02;
+  }
+  else if (argument == SeRIAL_7N1) {
+    return 0x04;
+  }
+  else if (argument == SeRIAL_8N1) {
+    return 0x06;
+  }
+  else if (argument == SeRIAL_5N2) {
+    return 0x08;
+  }
+  else if (argument == SeRIAL_6N2) {
+    return 0x0A;
+  }
+  else if (argument == SeRIAL_7N2) {
+    return 0x0C;
+  }
+  else if (argument == SeRIAL_8N2) {
+    return 0x0E;
+  }
+  else if (argument == SeRIAL_5E1) {
+    return 0x20;
+  }
+  else if (argument == SeRIAL_6E1) {
+    return 0x22;
+  }
+  else if (argument == SeRIAL_7E1) {
+    return 0x24;
+  }
+  else if (argument == SeRIAL_8E1) {
+    return 0x26;
+  }
+  else if (argument == SeRIAL_5E2) {
+    return 0x28;
+  }
+  else if (argument == SeRIAL_6E2) {
+    return 0x2A;
+  }
+  else if (argument == SeRIAL_7E2) {
+    return 0x2C;
+  }
+  else if (argument == SeRIAL_8E2) {
+    return 0x2E;
+  }
+  else if (argument == SeRIAL_5O1) {
+    return 0x30;
+  }
+  else if (argument == SeRIAL_6O1) {
+    return 0x32;
+  }
+  else if (argument == SeRIAL_7O1) {
+    return 0x34;
+  }
+  else if (argument == SeRIAL_8O1) {
+    return 0x36;
+  }
+  else if (argument == SeRIAL_5O2) {
+    return 0x38;
+  }
+  else if (argument == SeRIAL_6O2) {
+    return 0x3A;
+  }
+  else if (argument == SeRIAL_7O2) {
+    return 0x3C;
+  }
+  else if (argument == SeRIAL_8O2) {
+    return 0x3E;
+  }
 }
